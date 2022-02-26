cb2fd57fec3133513e58c47224d71f5ae05dcf1f
evelynfoy <evelyn.foy@hotmail.com>
2022-02-26 07:48:04 +0000

    Initial Commit

--- reduce.js
+++ reduce.js
@@ -6,7 +6,16 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce( (acc, cur) => {
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", cur,
+    "Total:", acc + cur
+    );
+  return acc + cur;
+}, 10);
+console.log(sum)
 
 const teamMembers = [
   {
@@ -32,6 +41,19 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let total = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience,0);
+console.log(total);
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  }
+  else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc
+    
+}, {});
+console.log(experienceByProfession);
